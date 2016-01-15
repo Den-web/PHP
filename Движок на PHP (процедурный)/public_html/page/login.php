6eae0bede2b24a347d788ba7ac1bccb188f3c2ba
Den-web <kirevden@gmail.com>
2016-01-15 19:28:10 +0200

    Php движок в процедурном стиле

---
+++ Движок на PHP (процедурный)/public_html/page/login.php
@@ -0,0 +1,27 @@
+<?php 
+if ($_POST['enter']) {
+echo 'Запрос...';
+exit;
+}
+
+Head('Вход') ?>
+<body>
+<div class="wrapper">
+<div class="header"></div>
+<div class="content">
+<?php Menu();
+MessageShow() 
+?>
+<div class="Page">
+<form method="POST" action="/login">
+<br><input type="text" name="login" placeholder="Логин" required>
+<br><input type="password" name="password" placeholder="Пароль" required>
+<br><br><input type="submit" name="enter" value="Вход"> <input type="reset" value="Очистить">
+</form>
+</div>
+</div>
+
+<?php Footer() ?>
+</div>
+</body>
+</html>
---
+++ Движок на PHP (процедурный)/public_html/setting.php
@@ -0,0 +1,10 @@
+<?php 
+
+define ('HOST', 'mysql.0hosting.me');
+define ('USER', 'u237339849_u1');
+define ('PASS', '100690');
+define ('DB', 'u237339849_lol');
+
+
+
+?>
